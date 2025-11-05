21ee63bcf9ce892891ebb8c58b83c27e24ff7812
putraborz <putra.myapi@gmail.com>
2025-11-05 17:54:42 +0700

    Update 21ll.lua

--- Loader/21ll.lua
+++ Loader/21ll.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/putraborz/lexhostlkp/refs/heads/main/Loader/FE%20Trolling%20GUI%20-%20Edited.luau"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/putraborz/lexhostlkp/refs/heads/main/Loader/FE%20Trolling%20GUI.lua"))()
